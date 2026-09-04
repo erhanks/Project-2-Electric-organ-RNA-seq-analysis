6f68f7eb28fd62fcd2ebe2e77ddb1ee92f520e76
Pen Hanks <penhanks@login2.talapas.uoregon.edu>
2026-09-04 00:02:46 -0700

    Piped the outputs and commented out the sam parser

--- Project2_QAA/Project2_Part3/htseq_counting.sh
+++ Project2_QAA/Project2_Part3/htseq_counting.sh
@@ -7,15 +7,16 @@
 #SBATCH --mem=64G
 
 SAM=/projects/bgmp/penhanks/bioinfo/Bi621/PS/erhanks-Bi621-PS8/sam_parser.py
-GTF=/projects/bgmp/penhanks/bioinfo/Bi623/Projects/Project-2-Electric-organ-RNA-seq-analysis/Project2_QAA/Project2_Part3/campylomormyrus.gtf
+GFF=/projects/bgmp/penhanks/bioinfo/Bi623/Projects/Project-2-Electric-organ-RNA-seq-analysis/Project2_QAA/Project2_Part3/campylomormyrus.gff
 
-CRH=/
-CCOX=/
-pixi run $SAM -i $CRH
-pixi run $SAM -i $CCOX
+CRH=/projects/bgmp/penhanks/bioinfo/Bi623/Projects/Project-2-Electric-organ-RNA-seq-analysis/Project2_QAA/Project2_Part3/Ccrh_outputAligned.out.sam
+CCOX=/projects/bgmp/penhanks/bioinfo/Bi623/Projects/Project-2-Electric-organ-RNA-seq-analysis/Project2_QAA/Project2_Part3/Ccox_outputAligned.out.sam
 
-pixi run htseq-count --stranded=yes $CRH $GTF
-pixi run htseq-count --stranded=yes $CCOX $GTF
+#pixi run $SAM -i $CRH
+#pixi run $SAM -i $CCOX
 
-pixi run htseq-count --stranded=reverse $CRH $GTF
-pixi run htseq-count --stranded=reverse $CCOX $GTF
+/usr/bin/time -v pixi run htseq-count --stranded=yes -i Parent $CRH $GFF > Crh_rhy51_EO_6cm_1_htseqcounts_yesstranded.txt
+/usr/bin/time -v pixi run htseq-count --stranded=yes -i Parent $CCOX $GFF > CcoxCrh_comrhy114_EO_adult_1_htseqcounts_yesstranded.txt
+
+/usr/bin/time -v pixi run htseq-count --stranded=reverse -i Parent $CRH $GFF > Crh_rhy51_EO_6cm_1_htseqcounts_revstranded.txt
+/usr/bin/time -v pixi run htseq-count --stranded=reverse -i Parent $CCOX $GFF > CcoxCrh_comrhy114_EO_adult_1_htseqcounts_revstranded.txt
